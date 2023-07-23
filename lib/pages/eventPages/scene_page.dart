8915d9e100dbc24a3582881ae6339f67cd2a0414
canarslandev <canarslanx@gmail.com>
2023-07-23 23:29:27 +0300

    Changes to be committed: 	modified:   lib/pages/eventPages/scene_page.dart  Changes not staged for commit: 	modified:   README.md 	modified:   analysis_options.yaml 	modified:   lib/controller/add_scene_page_controller.dart 	modified:   lib/controller/device_detail_page_controller.dart 	modified:   lib/controller/home_page_controller.dart 	modified:   lib/controller/main_page_controller.dart 	modified:   lib/controller/pair_device_page.dart 	modified:   lib/controller/sign_in_controller.dart 	modified:   lib/controller/sign_up_controller.dart 	modified:   lib/main.dart 	modified:   lib/pages/authentication/sign_in_page.dart 	modified:   lib/pages/authentication/sign_up_page.dart 	modified:   lib/pages/eventPages/add_scene_page.dart 	modified:   lib/pages/eventPages/device_detail_page.dart 	modified:   lib/pages/eventPages/pair_device_page.dart 	modified:   lib/pages/main_pages/account_page.dart 	modified:   lib/pages/main_pages/home_page.dart 	modified:   lib/pages/main_pages/main_page.dart 	modified:   lib/routes/pages.dart 	modified:   lib/routes/routes.dart 	modified:   lib/services/auth_service.dart 	modified:   lib/services/device_service.dart 	modified:   lib/services/firebase_service.dart 	modified:   lib/services/storage_service.dart 	modified:   lib/ui_alerts/basic_information_dialog.dart  Untracked files: 	lib/builders/scene_page_scene_builder.dart

--- lib/pages/eventPages/scene_page.dart
+++ lib/pages/eventPages/scene_page.dart
@@ -1,14 +1,10 @@
-// ignore_for_file: public_member_api_docs
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_iot_energy/builders/scene_page_scene_builder.dart';
 import 'package:flutter_iot_energy/controller/scene_page_controller.dart';
+import 'package:flutter_iot_energy/ui/text_style.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/src/simple/get_view.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
-
-import '../../ui/text_style.dart';
 
 class ScenePage extends GetView<ScenePageController> {
   const ScenePage({super.key});
@@ -23,49 +19,12 @@ class ScenePage extends GetView<ScenePageController> {
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 5.w),
           child: ListView(
-            children: [appbar, sceneWidget, sceneWidget],
+            children: [appbar, const BuilderScenePageScene()],
           ),
         ),
       ),
     );
   }
-
-  Widget get sceneWidget => Container(
-        padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 3.w),
-        margin: EdgeInsets.symmetric(vertical: 1.w),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color:
-                Theme.of(Get.context!).colorScheme.onTertiary.withOpacity(0.3),
-          ),
-          borderRadius: BorderRadius.circular(20),
-          color: Theme.of(Get.context!).colorScheme.onBackground,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(
-              width: 60.w,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Wake up plan",
-                    overflow: TextOverflow.ellipsis,
-                    style: pageTitleTextStyle.copyWith(fontSize: 5.5.w),
-                  ),
-                  Text(
-                    "Clock 23:20",
-                    style: pageDescriptionTextStyle.copyWith(fontSize: 3.8.w),
-                  )
-                ],
-              ),
-            ),
-            CupertinoSwitch(value: true, onChanged: (value) {}),
-          ],
-        ),
-      );
 
   Widget get appbar => Padding(
         padding: EdgeInsets.only(bottom: 3.w),
